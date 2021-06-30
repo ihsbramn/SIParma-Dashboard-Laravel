55f435f7d4f7d90e4598182045b1e889decc786f
ihsbramn <abdurahmanihsan25@gmail.com>
2021-06-30 20:37:48 +0700

    add filter method at jenis order

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,3 +27,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('report', ReportController::class);
+Route::get('/filter/home', [HomeController::class, 'filter'])->name('filter.home');
