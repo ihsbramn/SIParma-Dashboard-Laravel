23075677e33a843903b8dbf41c0862fda4f6064f
ihsbramn <abdurahmanihsan25@gmail.com>
2021-07-23 18:00:28 +0700

    bot token add to .env | app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'SIParma'),
 
     /*
     |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ return [
     // bot token
     'token' => env('BOT_TOKEN'),
     // bot token
-    
+
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
