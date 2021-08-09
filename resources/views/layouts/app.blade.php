c43933fdee3480e3532e5a5681d33495b394cbc9
rizkyfadilah22 <rizkyfadilah22@gmail.com>
2021-08-09 20:50:34 +0700

    finishing bismillah

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav nav-left">
                             <li class="nav-item {{ Route::currentRouteNamed('home') ? 'active' : '' }}">
-                                <a class="nav-link {{ Route::currentRouteNamed('home.*', 'home') ? 'active' : '' }}"
+                                <a class="nav-link {{ Route::currentRouteNamed('home.*', 'home',) ? 'active' : '' }}"
                                     aria-current="page" href="{{ route('home') }}">Dashboard</a>
                             </li>
                             @auth
@@ -106,7 +106,7 @@
                                         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">|</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link {{ Route::currentRouteNamed('user.*') ? 'active' : '' }}"
+                                        <a class="nav-link {{ Route::currentRouteNamed('register') ? 'active' : '' }}"
                                             href="{{ route('register') }}">Register User</a>
                                     </li>
                                 @endif
@@ -120,19 +120,12 @@
                     <ul class="navbar-nav">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
+                            {{-- @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link {{ Route::currentRouteNamed('login') ? 'active' : '' }}"
                                         href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link {{ Route::currentRouteNamed('register') ? 'active' : '' }}"
-                                        href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                            @endif --}}
                         @else
                             <li class="nav-item">
                                 <a class="nav-link active" href="#">Hi, {{ Auth::user()->name }}</a>
@@ -140,79 +133,55 @@
 
                             <li class="nav-item">
                                 @auth
-
                                     <a style="margin-left: 15px" class="btn btn-outline-light" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                                                                                                                                                                                                                                                                                                                                                                    document.getElementById('logout-form').submit();">
+                                                                                                                                                                                                                                                                                                                                                                                                                                            document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}</a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-
                                 @endauth
                             </li>
-
-                            {{-- <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    Hi, {{ Auth::user()->name }}
-                                </a>
-
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    @auth
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                            {{ __('Logout') }}
-                                        </a> --}}
-                            {{-- @if (Auth::user()->admin == 1)
-                                            <a class="dropdown-item" href="{{ route('report.index') }}">
-                                                Report
-                                            </a>
-                                        @endif --}}
-                            {{-- <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    @endauth
-
-                                </div>
-                            </li> --}}
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
 
-        @if (Route::currentRouteNamed('report.*'))
-            <nav class="navbar navbar-expand-lg py-0 navbar-dark bg-secondary">
-                <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav mx-auto">
-                            <li class="nav-item">
-                                <a class="nav-link {{ Route::currentRouteNamed('report.index', 'report.search*', 'report.datefilter') ? 'active' : '' }} py-0"
-                                    href="{{ route('report.index') }}">all</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link {{ Route::currentRouteNamed('report.open', 'report.datefilter.open*') ? 'active' : '' }} py-0"
-                                    href="{{ url('report.open') }}">open</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link {{ Route::currentRouteNamed('report.ogp', 'report.datefilter.ogp*') ? 'active' : '' }} py-0"
-                                    href="{{ url('report.ogp') }}">ogp</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link {{ Route::currentRouteNamed('report.eskalasi', 'report.datefilter.eskalasi*') ? 'active' : '' }} py-0"
-                                    href="{{ url('report.eskalasi') }}">eskalasi</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link {{ Route::currentRouteNamed('report.closed', 'report.datefilter.closed*') ? 'active' : '' }} py-0"
-                                    href="{{ url('report.closed') }}">closed</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        @endif
+        @auth
+            @if (Auth::user()->admin == 1)
+                @if (Route::currentRouteNamed('report.*'))
+                    <nav class="navbar navbar-expand-lg py-0 navbar-dark bg-secondary">
+                        <div class="container-fluid">
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav mx-auto">
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ Route::currentRouteNamed('report.index', 'report.search*', 'report.datefilter') ? 'active' : '' }} py-0"
+                                            href="{{ route('report.index') }}">all</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ Route::currentRouteNamed('report.open', 'report.datefilter.open*') ? 'active' : '' }} py-0"
+                                            href="{{ url('report.open') }}">open</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ Route::currentRouteNamed('report.ogp', 'report.datefilter.ogp*') ? 'active' : '' }} py-0"
+                                            href="{{ url('report.ogp') }}">ogp</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ Route::currentRouteNamed('report.eskalasi', 'report.datefilter.eskalasi*') ? 'active' : '' }} py-0"
+                                            href="{{ url('report.eskalasi') }}">eskalasi</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ Route::currentRouteNamed('report.closed', 'report.datefilter.closed*') ? 'active' : '' }} py-0"
+                                            href="{{ url('report.closed') }}">closed</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                @endif
+            @endif
+        @endauth
 
         <main>
             @yield('content')
